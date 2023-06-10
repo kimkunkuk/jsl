10dbe2a6837077656e57e8ba47b7268e36c1326e
Jukchu Lee <l0501jh@naver.com>
2023-06-10 19:56:49 +0900

    jjj

--- web_servlet_bike2/src/controller/Notice.java
+++ web_servlet_bike2/src/controller/Notice.java
@@ -28,7 +28,7 @@ public class Notice extends HttpServlet {
      */
     public Notice() {
         super();
-        // TODO Auto-generated constructor stub
+        // TODO Auto-generated  constructor stub
     }
 
 	/**
